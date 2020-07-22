199c950e2a67c0811848c638a09aa972ce640598
CTingCTer <gmiller290488@gmail.com>
2020-07-22 15:01:22 +0100

    Initial Commit

---
+++ Wordr-ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Wordr-ios
+//
+//  Created by Spare on 22/07/2020.
+//  Copyright © 2020 Spare. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
